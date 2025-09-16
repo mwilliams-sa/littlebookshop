36be7cef58af7147ba85cf14951038456efd363c
mwilliams-sa <mwilliams@sygnia.co.za>
2025-09-16 13:09:12 +0200

    router.js

---
+++ router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory }
+  from "https://unpkg.com/vue-router@4/dist/vue-router.esm-browser.js";
+
+import Home from "./home.js";
+import About from "./about.js";
+import Contact from "./contact.js";
+
+export default createRouter({
+  history: createWebHashHistory(), // works on GitHub Pages
+  routes: [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+    { path: "/contact", component: Contact },
+    { path: "/:pathMatch(.*)*", redirect: "/" },
+  ],
+});
