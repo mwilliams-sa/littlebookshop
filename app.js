2d355df71c44787c836c457e55197308db34dfbd
mwilliams-sa <mwilliams@sygnia.co.za>
2025-09-16 13:12:24 +0200

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
-import router from "./router.js"; // must match the renamed file
+import router from "./router.js";
 
 const App = { template: `<router-view></router-view>` };
 createApp(App).use(router).mount("#app");
