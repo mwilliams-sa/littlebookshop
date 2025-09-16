f2df420be79df23d70efa5f937758a4905380341
mwilliams-sa <mwilliams@sygnia.co.za>
2025-09-16 13:02:14 +0200

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("app.js loaded");
+
+// EXACT line (CDN), not "vue"
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import router from "./router.js";
+
+const App = { template: `<router-view></router-view>` };
+createApp(App).use(router).mount("#app");
